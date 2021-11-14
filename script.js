02cb7a86505142c5a0dc21918b378e8021a8813e
Sovan Charan <59173364+sovancharan@users.noreply.github.com>
2021-11-14 13:19:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var names =new Array();
 names[0]="Yaakov";
-names[1]="jon";
-names[2]="jen";
-names[3]="sovan";
-names[4]="priya";
-names[5]="pritam";
-names[6]="papai";
-names[7]="james";
-names[8]="souvick";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 for (var index = 0; index < names.length; index++) {
   //  console.log(names[index]);
